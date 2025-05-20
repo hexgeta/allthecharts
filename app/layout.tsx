0a9de99fe4bc885a40621352e1d83c0d25452db1
hexgeta <hexgeta@proton.me>
2025-05-20 10:13:56 +0100

    auto-commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,7 +77,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="font-sans">
+    <html lang="en" className="">
       <head>
         <FontLoader weight="bold" />
         <script defer data-domain="allthecharts.com" src="https://plausible.io/js/script.js"></script>        <FontLoader weight="regular" priority={true} />
